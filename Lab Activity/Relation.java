b46c4862933dc7608afb7a5390e0bbae97c6a456
SHUBHAM3701 <107395150+SHUBHAM3701@users.noreply.github.com>
2022-06-17 12:52:32 +0530

    Add files via upload

---
+++ Lab Activity/Relation.java
@@ -0,0 +1,18 @@
+//Relational Operators are used to check the relationship between two operands.
+// a = b; // b value is assigend to a.
+// a == b; // it checks whether the value of a is equals to b or not.
+
+class Operator{
+  public static void main(string aargs[])
+ {
+     int a = 11;
+     int b = 11;
+
+   System.out.println(a==b);
+   System.out.println(a!=b);
+   System.out.println(a>b);
+   System.out.println(a<b);
+   System.out.println(a>=b);
+   System.out.println(a<=b);
+ }
+}
