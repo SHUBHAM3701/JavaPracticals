4d0fd3256aed51e5210b637faff7916ada9651c0
SHUBHAM3701 <107395150+SHUBHAM3701@users.noreply.github.com>
2022-07-27 14:15:23 +0530

    Add files via upload

---
+++ Practical/Lexicographical.java
@@ -0,0 +1,13 @@
+public class Lexicographical {
+
+    public static void main(String[] args){
+	   
+	   String str = new String("Welcome to java world");
+	   System.out.println(str.charAt (5));
+	   System.out.println(str.concat (" lest learn java"));
+	   System.out.println(str.indexOf ("a"));
+	   System.out.println(str.replaceAll ("a", "e"));
+	   
+	}
+	
+}	
