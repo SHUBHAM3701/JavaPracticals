eac35b4dc74be9cd03bd20665c0734a8160534a8
SHUBHAM3701 <107395150+SHUBHAM3701@users.noreply.github.com>
2022-07-27 14:13:15 +0530

    Add files via upload

---
+++ Practical/Circle.java
@@ -0,0 +1,17 @@
+ 
+
+class Circle {
+               float radius;
+              
+               Circle() {
+                            radius = 1.5f;
+                         }
+          
+                }
+   public class Shape {
+                        
+                       public static void main(String[] args) {
+                              Circle circle1 = new Circle();
+                              System.out.println("The Radius of the Circle is :" + circle.radius);
+                          }
+                }
