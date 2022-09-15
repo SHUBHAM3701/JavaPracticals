be692e557d9b6306b444b890e3a8d4501faff6c5
SHUBHAM3701 <107395150+SHUBHAM3701@users.noreply.github.com>
2022-09-15 13:05:14 +0530

    Add files via upload

---
+++ Assignment/CountryMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class CountryMap {
+
+	public static void main(String[] args) {
+		CountryMap countryMap = new CountryMap();
+
+		countryMap.saveCountryCapital("India", "Delhi");
+		countryMap.saveCountryCapital("Japan", "Tokyo");
+		
+		
+		System.out.println(countryMap.getCapital("India"));
+		System.out.println(countryMap.getCountry("Tokyo"));
+		System.out.println(countryMap.toArrayList());
+		
+		HashMap<String, String> M2 = countryMap.swapKyeValue();
+		System.out.println(M2);
+	}
+
+}
